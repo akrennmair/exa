bd59904b910bcc849f26359c0e79d89cba5c9f24
Andreas Krennmair <ak@synflood.at>
2021-04-11 15:54:47 +0200

    add a few more unit tests.

--- buffer_test.go
+++ buffer_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRuneIndex(t *testing.T) {
 	testData := map[string]struct {
@@ -48,9 +52,114 @@ func TestRuneIndex(t *testing.T) {
 	for testName, tt := range testData {
 		t.Run(testName, func(t *testing.T) {
 			idx := runeIndex(tt.Haystack, tt.Needle)
-			if idx != tt.ExpectedIndex {
-				t.Fatalf("haystack %q, needle %q: expected index %d, got %d", string(tt.Haystack), string(tt.Needle), tt.ExpectedIndex, idx)
-			}
+			assert.Equal(t, tt.ExpectedIndex, idx, "haystack %q, needle %q", string(tt.Haystack), string(tt.Needle))
+		})
+	}
+}
+
+func TestGetSelection(t *testing.T) {
+	testData := map[string]struct {
+		buf             *buffer
+		expectedLowerY  int
+		expectedLowerX  int
+		expectedHigherY int
+		expectedHigherX int
+	}{
+		"simple": {
+			buf:             &buffer{startY: 0, startX: 23, endY: 3, endX: 42},
+			expectedLowerY:  0,
+			expectedLowerX:  23,
+			expectedHigherY: 3,
+			expectedHigherX: 42,
+		},
+		"reversed": {
+			buf:             &buffer{startY: 3, startX: 23, endY: 0, endX: 42},
+			expectedLowerY:  0,
+			expectedLowerX:  42,
+			expectedHigherY: 3,
+			expectedHigherX: 23,
+		},
+		"same-line-reversed": {
+			buf:             &buffer{startY: 0, startX: 15, endY: 0, endX: 3},
+			expectedLowerY:  0,
+			expectedLowerX:  3,
+			expectedHigherY: 0,
+			expectedHigherX: 15,
+		},
+	}
+
+	for testName, tt := range testData {
+		t.Run(testName, func(t *testing.T) {
+			lowerY, lowerX, higherY, higherX := tt.buf.getSelection()
+			assert.Equal(t, tt.expectedLowerY, lowerY)
+			assert.Equal(t, tt.expectedLowerX, lowerX)
+			assert.Equal(t, tt.expectedHigherY, higherY)
+			assert.Equal(t, tt.expectedHigherX, higherX)
+		})
+	}
+}
+
+func TestIsWithinSelectedText(t *testing.T) {
+	testData := map[string]struct {
+		buf            *buffer
+		y              int
+		x              int
+		expectedResult bool
+	}{
+		"simple": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              2,
+			x:              3,
+			expectedResult: true,
+		},
+		"simple-first-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              1,
+			x:              10,
+			expectedResult: true,
+		},
+		"simple-last-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              3,
+			x:              7,
+			expectedResult: true,
+		},
+		"outside-first-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              1,
+			x:              0,
+			expectedResult: false,
+		},
+		"outside-last-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              3,
+			x:              20,
+			expectedResult: false,
+		},
+		"outside-before-first-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              0,
+			x:              20,
+			expectedResult: false,
+		},
+		"outside-after-last-line": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 3, endX: 10},
+			y:              5,
+			x:              3,
+			expectedResult: false,
+		},
+		"empty-selection": {
+			buf:            &buffer{startY: 1, startX: 5, endY: 1, endX: 5},
+			y:              1,
+			x:              5,
+			expectedResult: false,
+		},
+	}
+
+	for testName, tt := range testData {
+		t.Run(testName, func(t *testing.T) {
+			result := tt.buf.isWithinSelectedText(tt.y, tt.x)
+			assert.Equal(t, tt.expectedResult, result)
 		})
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/akrennmair/exa
 go 1.16
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gdamore/tcell/v2 v2.2.0
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.10
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20210324051608-47abb6519492 // indirect
 	golang.org/x/term v0.0.0-20210317153231-de623e64d2a6 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
